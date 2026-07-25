848f59b229635a2694a80afdbe62d3a725bbcf2c
amir141034 <64488465+amir141034@users.noreply.github.com>
2026-07-25 22:30:34 +0800

    refactor: change map for a better street view

--- src/components/GpsMap.vue
+++ src/components/GpsMap.vue
@@ -12,8 +12,9 @@ const mapContainer = ref(null)
 onMounted(() => {
   const map = L.map(mapContainer.value).setView([3.139, 101.6869], 13) // Kuala Lumpur
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors',
+  L.tileLayer('https://api.maptiler.com/maps/streets-v4/{z}/{x}/{y}.png?key=CY4Ca6jn71nbcvMdzH8x', {
+    attribution:
+      '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
   }).addTo(map)
 
   L.marker([3.139, 101.6869]).addTo(map).bindPopup('I am here')
